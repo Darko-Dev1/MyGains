2a22780fdfc0a3ac4bdd15f3655ac7a42db98bd8
Darko-Dev1 <kitanoskibest@gmail.com>
2025-01-30 16:47:28 +0100

    pls work

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
+const path = require('path');
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find the views (EJS templates)
+app.set('views', path.join(__dirname, '../views'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Render the EJS template
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index');  // Make sure 'index.ejs' exists in the 'views' folder
 });
 
+// Export the app for Vercel to handle requests
 module.exports = app;
